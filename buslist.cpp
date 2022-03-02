e63b6e39360ac36d7a267fe07093ccc857d0256f
Wolfgang Grote-Ramm <wolfgang.grote@hs-duesseldorf.de>
2022-03-03 00:03:08 +0100

    conversion from bus integer values done

--- buslist.cpp
+++ buslist.cpp
@@ -18,58 +18,66 @@
 
 #define FILENAME "buslist.csv"
 #define BUF 4096
+#define PN_RECVSIZE 1440
+#define PN_SENDSIZE 1440
+
+#define MAX_I 20000				// maximum integer number of measurement value (20mA)
+#define MIN_I 4000				// maximum integer number of measurement value (4mA)
+#define TOLERANCE 0.0			// relative tolerance for out-of-range detection of measurement
 
 struct busObj bus[1440];
-
-char* replace_char(char* str, char find, char replace);
-
-int16_t getBEint16(int16_t *bigEndianNumber);
-
-uint16_t getBEuint16(uint16_t *bigEndianNumber);
-
-uint32_t getBEuint32(uint32_t *bigEndianNumber);
-
-float getBEreal(float *bigEndianNumber);
-
-bool getBit(uint8_t bitArray, int n);
-
-char *printBitSequence(void *var, size_t bytes, char *strng);
-
-template <typename T>
-T swap_endian(T u) {
-    static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
-
-    union {
-        T u;
-        uint8_t u8[sizeof(T)];
-    } source, dest;
-
-    source.u = u;
-
-    for (size_t k = 0; k < sizeof(T); k++)
-        dest.u8[k] = source.u8[sizeof(T) - k - 1];
-
-    return dest.u;
-}
-
-template <typename T>
-void print_hex(T input) {
-	size_t i;
-	union {
-		T input;
-		uint8_t array[sizeof(T)];
-	} field;
-	field.input = input;
-	for (i=0; i<sizeof(T); i++){
-		printf("%x",field.array[i]);
-	}
-}
+uint8_t PNsend[PN_SENDSIZE]={0}, PNrecv[PN_RECVSIZE]={0};
 
 
 int main(void) {
+	int i;
+	readBuslist(FILENAME, bus, 1440);
+	i = 0;
+	do {
+		printf("A[%3d] = %3d, %s, %s, %4s, %lf, %lf, %s, %3d, %d->%d.%d\n", i, bus[i].num, bus[i].tag, bus[i].name, bus[i].ctrdef, bus[i].llMeas, bus[i].ulMeas, bus[i].unit, bus[i].datatype, bus[i].write, bus[i].startByte, bus[i].bit);
+		i++;
+	}
+	while (bus[i].num > bus[i-1].num && i < 1440);
+	printf("float has %lu bits\n",sizeof(float)*CHAR_BIT);
+	int16_t num = 12345;
+	uint16_t num2 = 65000;
+	uint32_t num3 = 0xFFFFFFF1;
+	float num4 = 12.888;
+	printf("%d,\t\t %d\n", num, getBEint16(&num));
+	printf("%x,\t\t %x\n", num, getBEint16(&num));
+	printf("%d,\t\t %d\n", num2, getBEuint16(&num2));
+	printf("%x,\t\t %x\n", num2, getBEuint16(&num2));
+	printf("%u,\t\t %u\n", num3, getBEuint32(&num3));
+	printf("%x,\t\t %x\n", num3, getBEuint32(&num3));
+	printf("%f,\t\t %f\n", num4, getBEreal(&num4));
+	print_hex(num4);
+	printf(",\t\t ");
+	print_hex(getBEreal(&num4));
+	printf("\n");
+	printf("%f,\t\t %f\n", num4, swap_endian<float>(num4));
+	print_hex(num4);
+	printf(",\t\t ");
+	print_hex(swap_endian<float>(num4));
+	printf("\n");
+	uint8_t bitArray = 0x1;
+	for (i=7; i>=0; i--){
+		printf("%d ",getBit(bitArray, i));
+	}
+	char buffer3[65];
+	//printBitSequence((void*) &num2, 2, buffer3);
+	printf("\nVariable in decimal: %u, variable binary. %s\n", num3, printBitSequence((void*) &num3, 4, buffer3));
+	printf("\n");
+	readBus(bus, PNrecv, 100);
+	for (i=0; i<100; i++){
+		printf("%s = %lf or in int: %u \n",bus[i].name, bus[i].dval, bus[i].busval);
+	}
+	printf("Closing...\n");
+	return EXIT_SUCCESS;
+}
+
+int readBuslist(const char *busfile, struct busObj *bus, int n){
 	FILE *fp;
 	char strbuf1[BUF], strbuf2[BUF];
-	char busfile[] = {FILENAME};
 	char *tok, *cptr;
 	int k, i, j;
 	uint16_t offsetAddr = 0;
@@ -78,7 +86,7 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 	i=0;
-	while (fgets(strbuf1, BUF, fp)){	//row-wise extraction of strings
+	while (fgets(strbuf1, BUF, fp) && i<n){	//row-wise extraction of strings
 	  tok = strtok(strbuf1,";");
 	  k = 1;
 	  if (isdigit((int)tok[0])){
@@ -124,9 +132,9 @@ int main(void) {
 				break;
 			case 11: 	// start address
 				if (tok[0] == 'A'){ // AW means input / read for MPC:
-					bus[i].rw = false;
+					bus[i].write = false;
 				}
-				else bus[i].rw = true; // output / write
+				else bus[i].write = true; // output / write
 				bus[i].startByte = (uint16_t) strtol(tok+2, NULL, 0);
 				if (i==0) offsetAddr = bus[i].startByte;
 				bus[i].startByte -= offsetAddr;	// subtract first address value
@@ -140,45 +148,97 @@ int main(void) {
 		  i++;
 	  }
 	}
-	i = 0;
-	do {
-		printf("A[%3d] = %3d, %s, %s, %lf, %lf, %s, %3d, %d->%d.%d\n", i, bus[i].num, bus[i].tag, bus[i].name, bus[i].llMeas, bus[i].ulMeas, bus[i].unit, bus[i].datatype, bus[i].rw, bus[i].startByte, bus[i].bit);
-		i++;
-	}
-	while (bus[i].num > bus[i-1].num && i < 1440);
-	printf("float has %lu bits\n",sizeof(float)*CHAR_BIT);
-	int16_t num = 12345;
-	uint16_t num2 = 65000;
-	uint32_t num3 = 0xFFFFFFF1;
-	float num4 = 12.888;
-	printf("%d,\t\t %d\n", num, getBEint16(&num));
-	printf("%x,\t\t %x\n", num, getBEint16(&num));
-	printf("%d,\t\t %d\n", num2, getBEuint16(&num2));
-	printf("%x,\t\t %x\n", num2, getBEuint16(&num2));
-	printf("%u,\t\t %u\n", num3, getBEuint32(&num3));
-	printf("%x,\t\t %x\n", num3, getBEuint32(&num3));
-	printf("%f,\t\t %f\n", num4, getBEreal(&num4));
-	print_hex(num4);
-	printf(",\t\t ");
-	print_hex(getBEreal(&num4));
-	printf("\n");
-	printf("%f,\t\t %f\n", num4, swap_endian<float>(num4));
-	print_hex(num4);
-	printf(",\t\t ");
-	print_hex(swap_endian<float>(num4));
-	printf("\n");
-	uint8_t bitArray = 0x1;
-	for (i=7; i>=0; i--){
-		printf("%d ",getBit(bitArray, i));
-	}
-	char buffer3[65];
-	//printBitSequence((void*) &num2, 2, buffer3);
-	printf("\nVariable in decimal: %u, variable binary. %s\n", num3, printBitSequence((void*) &num3, 4, buffer3));
-	printf("\n");
-	printf("Closing...\n");
 	fclose(fp);
 	return EXIT_SUCCESS;
 }
+
+double buscode2double(struct busObj *bus, uint8_t *recvstream) {
+	// convert uint16 coded value to double value
+	double retval, a, b;
+	const int span = MAX_I-MIN_I;
+	char errormsg[200];
+	// read number from byte stream and put into busval:
+	bus->busval = (recvstream[bus->startByte] << 8) | (recvstream[bus->startByte+1]); // convert from big endian to to little endian
+	// coefficients for conversion to real world values:
+	a = (bus->ulMeas-bus->llMeas) / ((double) (MAX_I - MIN_I));
+	b = bus->llMeas - a*(double) MIN_I;
+	// check for channel error:
+	if (bus->busval==0) { // 0 is sent for submit channel error
+		if (bus->chn_err_trt < 0){ // use lower limit of measurement range
+			if (bus->chn_err_msg) {
+				sprintf(errormsg, "WARNING: Channel error in %s, %s: using lower limit of range", bus->tag, bus->name);
+				fprintf(stderr,"%s\n",errormsg);
+			}
+			bus->busval = MIN_I; // set to MIN_I
+			retval = a * (double) (bus->busval) + b;
+		}
+		else if (bus->chn_err_trt > 0) { // use upper limit of measurement range
+			if (bus->chn_err_msg) {
+				sprintf(errormsg, "WARNING: Channel error in %s, %s: using upper limit of range", bus->tag, bus->name);
+				fprintf(stderr,"%s\n",errormsg);
+			}
+			bus->busval = MAX_I; // set to MAX_I
+			retval = a * (double) (bus->busval) + b;
+		}
+		else { 					// set to default value
+			if (bus->chn_err_msg) {
+				sprintf(errormsg, "WARNING: Channel error in %s, %s: using nominal value", bus->tag, bus->name);
+				fprintf(stderr,"%s\n",errormsg);
+			}
+			retval = a * (double) (bus->nominalVal) + b;
+		}
+	}
+	// check for "out of range" and set to reasonable values:
+	else if (bus->busval < MIN_I - (uint16_t) (TOLERANCE*span)) {
+		bus->busval = MIN_I - (uint16_t) (TOLERANCE*span); // set to MIN_I - TOLERANCE*span
+		retval = a * (double) (bus->busval) + b;
+	}
+	else if (bus->busval > MAX_I + (uint16_t) (TOLERANCE*span)) {
+		bus->busval = MAX_I + (uint16_t) (TOLERANCE*span); // set to MAX_I + TOLERANCE*span
+		retval = a * (double) (bus->busval) + b;
+	}
+	else { // This is default...
+		retval = a * (double) (bus->busval) + b;
+	}
+	bus->dval = retval;
+	return retval;
+}
+
+int readBus(struct busObj *bus, uint8_t *recvstream, int n){
+	// function advances trough bus struct array and fills its respective values using the byte array recvstream
+	int i;
+
+	// loop over all n busObj and case-distinct between type of values
+	for (i=0; i<n; i++){
+		if (bus[i].write == false) { // it is a read variable:
+			switch (bus[i].datatype){
+			case 1:		// BOOL
+				bus[i].bval = getBit(recvstream[bus[i].startByte], bus[i].bit);
+				break;
+			case 2: 	// INT = int16_t
+				bus[i].ival = (int) getBEint16((int16_t*) recvstream[bus[i].startByte]); // TODO: check if correct
+				break;
+			case 3: 	// UINT = uint16_t
+				bus[i].ival = (int) getBEuint16((uint16_t*) recvstream[bus[i].startByte]); // TODO: check if correct
+				break;
+			case 4: 	// UDINT = uint32_t
+				bus[i].ival = (int) getBEuint32((uint32_t*) recvstream[bus[i].startByte]); // TODO: check if correct
+				break;
+			case 5: 	// Scaled Real
+				buscode2double(&bus[i], recvstream);
+				break;
+			case 6:		// REAL = 32 bit float
+				bus[i].dval = (double) getBEreal((float*) recvstream[bus[i].startByte]);
+				break;
+			default :
+				printf("WARNING: Case not existent in function readBus.\n");
+				return EXIT_FAILURE;
+			}
+		}
+	}
+	return EXIT_SUCCESS;
+}
+
 
 char* replace_char(char* str, char find, char replace){
     char *current_pos = strchr(str,find);
--- buslist.h
+++ buslist.h
@@ -14,9 +14,9 @@
 
 struct busObj { 				// Object for bus communication
 	int num; 					// Number of signal in bus list
-	char name[BUS_NAME_MAX];		// Name of variable
-	char tag[BUS_TAG_MAX];			// Tag no. of variable
-	char unit[BUS_UNIT_MAX];		// Physical unit of entity
+	char name[BUS_NAME_MAX];	// Name of variable
+	char tag[BUS_TAG_MAX];		// Tag no. of variable
+	char unit[BUS_UNIT_MAX];	// Physical unit of entity
 	int datatype;				// 1=BOOL, 2=INT, 3=UINT, 4=UDINT, 5=SCALED REAL, 6 = REAL
 	bool bval;					// Boolean value
 	int ival;					// Integer value
@@ -27,15 +27,65 @@ struct busObj { 				// Object for bus communication
 	double ulMeas;				// max value of measurement range corresponding to busval = 20,000
 	uint16_t startByte;			// start byte in stream
 	uint8_t	bit;				// bit in start byte for boolean values
-	bool rw;					// value read or write in this program: read=0, write=1
-	int channelErr;				// on channel error use: (-1: lower limit of range, 1: upper limit of range, 0: nominal value)
+	bool write;					// value read or write in this program: read=0, write=1
 	double nominalVal;			// nominal value
-	bool errMsg;				// display error message on screen on channel error
+	bool chn_err;				// channel error state
+	bool chn_err_msg;			// display error message on screen on channel error
+	int chn_err_trt;			// on channel error use: (-1: lower limit of range, 1: upper limit of range, 0: nominal value)
+	char ctrdef[5];				// definition of control quantity (u, w, y, d) followed by number (1, 2, ..) e.g. w3 for 3rd set point
 	// constructor:
-	busObj():num(0), name("empty"),tag("empty"),unit("1"),datatype(0),bval(false),ival(0),ulval(0),dval(0.0),busval(0),llMeas(0.0), ulMeas(1.0), startByte(0), bit(0), rw(false),channelErr(0), nominalVal(0.0), errMsg(false) { }
+	busObj():num(0), name("empty"),tag("empty"),unit("1"),datatype(0),bval(false),ival(0),ulval(0),dval(0.0),busval(0),llMeas(0.0), ulMeas(1.0), startByte(0), bit(0), write(false), nominalVal(0.0), chn_err(true), chn_err_msg(false), chn_err_trt(1), ctrdef("none") { }
 };
 
+int readBuslist(const char *busfile, struct busObj *bus, int n);
 
+double buscode2double(struct busObj *bus, uint8_t *recvstream);
+
+int readBus(struct busObj *bus, uint8_t *recvstream, int n);
+
+char* replace_char(char* str, char find, char replace);
+
+int16_t getBEint16(int16_t *bigEndianNumber);
+
+uint16_t getBEuint16(uint16_t *bigEndianNumber);
+
+uint32_t getBEuint32(uint32_t *bigEndianNumber);
+
+float getBEreal(float *bigEndianNumber);
+
+bool getBit(uint8_t bitArray, int n);
+
+char *printBitSequence(void *var, size_t bytes, char *strng);
+
+template <typename T>
+T swap_endian(T u) {
+    static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
+
+    union {
+        T u;
+        uint8_t u8[sizeof(T)];
+    } source, dest;
+
+    source.u = u;
+
+    for (size_t k = 0; k < sizeof(T); k++)
+        dest.u8[k] = source.u8[sizeof(T) - k - 1];
+
+    return dest.u;
+}
+
+template <typename T>
+void print_hex(T input) {
+	size_t i;
+	union {
+		T input;
+		uint8_t array[sizeof(T)];
+	} field;
+	field.input = input;
+	for (i=0; i<sizeof(T); i++){
+		printf("%x",field.array[i]);
+	}
+}
 
 
 #endif /* BUSLIST_H_ */
